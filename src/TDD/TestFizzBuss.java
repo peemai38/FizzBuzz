4b9d9ec9cb62ff3a829c38bc8015ff4fdcf766fd
thesspyy <punyanut.nong@kkumail.com>
2019-10-02 16:58:04 +0700

    FizzBuzz version2 : Input2 Expect2

--- src/TDD/TestFizzBuss.java
+++ src/TDD/TestFizzBuss.java
@@ -1,17 +1,24 @@
 package TDD;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class TestFizzBuss {
 	FizzBuss fizzbuss = new FizzBuss();
-	int input = fizzbuss.getinput(1);	
+	int input = fizzbuss.getinput(1);
+	int input2 = fizzbuss.getinput(2);
 	
 	@Test
 	@DisplayName("input 1 : expect 1")
 	void TC01() {	
 		assertEquals(1, input);
+	}
+	
+	@Test
+	@DisplayName("input 2 : expect 2")
+	void TC02() {	
+		assertEquals(2, input2);
 	}
 }
